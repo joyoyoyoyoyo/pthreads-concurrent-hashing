ae2f502cd7c6c1462df8bfd13d216710a3871e28
Angel Ortega <an22ortega@gmail.com>
2017-04-25 15:43:18 -0700

    updated test program

---
+++ phash.h
@@ -0,0 +1,34 @@
+//
+// Created by bahamut on 4/25/17.
+//
+
+#ifndef PTHREADS_CONCURRENT_HASHING_PHASH_H
+#define PTHREADS_CONCURRENT_HASHING_PHASH_H
+class LinkedHashEntry {
+private:
+  int key;
+  int value;
+  LinkedHashEntry *next;
+public:
+  LinkedHashEntry(int key, int value);
+  int getKey();
+  int getValue();
+  void setValue(int value);
+
+  LinkedHashEntry *getNext();
+  void setNext(LinkedHashEntry *next);
+};
+
+
+class HashMap {
+private:
+  LinkedHashEntry **table;
+public:
+  HashMap();
+  int get(int key);
+  void put(int key, int value);
+  void remove(int key);
+  ~HashMap();
+};
+
+#endif //PTHREADS_CONCURRENT_HASHING_PHASH_H
