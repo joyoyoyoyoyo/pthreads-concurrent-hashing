f6b1f587a4ba6e24beff1e09bd8a7051936513d8
Angel Ortega <an22ortega@gmail.com>
2017-04-27 20:11:23 -0700

    push code in from phashcoarse to phashfine

--- phash1.cc
+++ phash1.cc
@@ -1,9 +1,9 @@
-// Fine grain locking based concurrent hash table.
-// Created by Angel Ortega on 4/25/17.
-//
+// Course grain locking based concurrent hash table.
+// Created by Angel Ortega on 4/24/17.
 
-#include "phash.h"
 #include <iostream>
+#include "phash.h"
+
 
 LinkedHashEntry::LinkedHashEntry(int key, int value) {
   this->key = key;
@@ -40,7 +40,13 @@ LinkedHashEntry::setNext(LinkedHashEntry *next) {
 
 const int TABLE_SIZE = 128;
 
+
+
+#ifdef RWLOCK
+
+// Coarse Read-Write Lock
 HashMap::HashMap() {
+  lock = new RWLock();
   table = new LinkedHashEntry *[TABLE_SIZE];
   for (int i = 0; i < TABLE_SIZE; i++)
     table[i] = NULL;
@@ -48,39 +54,51 @@ HashMap::HashMap() {
 
 int
 HashMap::get(int key) {
+  lock->startRead();
   int hash = (key % TABLE_SIZE);
-  if (table[hash] == NULL)
+  if (table[hash] == NULL) {
+    lock->doneRead();
     return -1;
+  }
   else {
     LinkedHashEntry *entry = table[hash];
     while (entry != NULL && entry->getKey() != key)
       entry = entry->getNext();
-    if (entry == NULL)
+    if (entry == NULL) {
+      lock->doneRead();
       return -1;
-    else
+    }
+    else {
+      lock->doneRead();
       return entry->getValue();
+    }
   }
 }
 
 void
 HashMap::put(int key, int value) {
+  lock->startWrite();
   int hash = (key % TABLE_SIZE);
-  if (table[hash] == NULL)
+  if (table[hash] == NULL) {
     table[hash] = new LinkedHashEntry(key, value);
+    lock->doneWrite();
+  }
   else {
     LinkedHashEntry *entry = table[hash];
-    while (entry->getNext() != NULL)
+    while (entry->getNext() != NULL && entry->getKey() != key)
       entry = entry->getNext();
     if (entry->getKey() == key)
       entry->setValue(value);
     else
       entry->setNext(new LinkedHashEntry(key, value));
+    lock->doneWrite();
   }
 }
 
 
 void
 HashMap::remove(int key) {
+  lock->startWrite();
   int hash = (key % TABLE_SIZE);
   if (table[hash] != NULL) {
     LinkedHashEntry *prevEntry = NULL;
@@ -101,9 +119,11 @@ HashMap::remove(int key) {
       }
     }
   }
+  lock->doneWrite();
 }
 
 HashMap::~HashMap() {
+//  lock->startWrite();
   for (int i = 0; i < TABLE_SIZE; i++)
     if (table[i] != NULL) {
       LinkedHashEntry *prevEntry = NULL;
@@ -115,4 +135,107 @@ HashMap::~HashMap() {
       }
     }
   delete[] table;
+
+  // clean up our resources
+//  lock->doneWrite();
+//  delete lock;
 }
+
+#else
+// Coarse without Read-Write lock
+HashMap::HashMap() {
+  table = new LinkedHashEntry *[TABLE_SIZE];
+  for (int i = 0; i < TABLE_SIZE; i++)
+    table[i] = NULL;
+}
+
+int
+HashMap::get(int key) {
+  pthread_mutex_lock(&coarse_mutex);
+  int hash = (key % TABLE_SIZE);
+  if (table[hash] == NULL) {
+    pthread_mutex_unlock(&coarse_mutex);
+    return -1;
+  }
+  else {
+    LinkedHashEntry *entry = table[hash];
+    while (entry != NULL && entry->getKey() != key)
+      entry = entry->getNext();
+    if (entry == NULL) {
+      pthread_mutex_unlock(&coarse_mutex);
+      return -1;
+    }
+    else {
+      pthread_mutex_unlock(&coarse_mutex);
+      return entry->getValue();
+    }
+  }
+}
+
+void
+HashMap::put(int key, int value) {
+  pthread_mutex_lock(&coarse_mutex);
+  int hash = (key % TABLE_SIZE);
+  if (table[hash] == NULL) {
+    table[hash] = new LinkedHashEntry(key, value);
+    pthread_mutex_unlock(&coarse_mutex);
+  }
+  else {
+    LinkedHashEntry *entry = table[hash];
+    while (entry->getNext() != NULL && entry->getKey() != key)
+      entry = entry->getNext();
+    if (entry->getKey() == key)
+      entry->setValue(value);
+    else
+      entry->setNext(new LinkedHashEntry(key, value));
+    pthread_mutex_unlock(&coarse_mutex);
+  }
+}
+
+
+void
+HashMap::remove(int key) {
+  pthread_mutex_lock(&coarse_mutex);
+  int hash = (key % TABLE_SIZE);
+  if (table[hash] != NULL) {
+    LinkedHashEntry *prevEntry = NULL;
+    LinkedHashEntry *entry = table[hash];
+    while (entry->getNext() != NULL && entry->getKey() != key) {
+      prevEntry = entry;
+      entry = entry->getNext();
+    }
+    if (entry->getKey() == key) {
+      if (prevEntry == NULL) {
+        LinkedHashEntry *nextEntry = entry->getNext();
+        delete entry;
+        table[hash] = nextEntry;
+      } else {
+        LinkedHashEntry *next = entry->getNext();
+        delete entry;
+        prevEntry->setNext(next);
+      }
+    }
+  }
+  pthread_mutex_unlock(&coarse_mutex);
+}
+
+HashMap::~HashMap() {
+  pthread_mutex_lock(&coarse_mutex);
+  for (int i = 0; i < TABLE_SIZE; i++)
+    if (table[i] != NULL) {
+      LinkedHashEntry *prevEntry = NULL;
+      LinkedHashEntry *entry = table[i];
+      while (entry != NULL) {
+        prevEntry = entry;
+        entry = entry->getNext();
+        delete prevEntry;
+      }
+    }
+  delete[] table;
+
+  // clean up our resources
+  pthread_mutex_unlock(&coarse_mutex);
+  pthread_mutex_destroy(&coarse_mutex);
+  pthread_exit(NULL);
+}
+#endif
